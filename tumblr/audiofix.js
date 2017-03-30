7d074fb5465ae2a0cae503280b93980dbb97f8b2
adamasstokhorst <adamas.aqsa@gmail.com>
2017-03-30 14:15:28 +0800

    snip code

--- tumblr/audiofix.js
+++ tumblr/audiofix.js
@@ -2,7 +2,7 @@
  function audio() {
   x = document.getElementsByClassName('tumblr_audio_player');
   for (i = 0; i<x.length; i++) {
-	x[i].height = 31;
+	// x[i].height = 31;
 	y = x[i].contentDocument;
 	/* if (y.getElementsByClassName('audio-image').length > 0) {
 		y.getElementsByClassName('audio-image')[0].style.display = 'none';
@@ -15,8 +15,8 @@
 	y.getElementsByClassName('progress')[0].style.background = '#333';
 	/* y.getElementsByClassName('track-name')[0].style.display = 'none';
 	y.getElementsByClassName('track-artist')[0].style.display = 'none';
-	y.getElementsByClassName('play-pause')[0].style.fontSize = '20px';
+	y.getElementsByClassName('play-pause')[0].style.fontSize = '20px'; */
 	y.getElementsByClassName('play-pause')[0].style.color = '#fff';
-	y.getElementsByClassName('audio-info')[0].style.color = '#fff'; */
+	y.getElementsByClassName('audio-info')[0].style.color = '#fff';
    }
   }
